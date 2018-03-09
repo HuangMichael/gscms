2e29e07b5955bb12574dbafec4803fc5754a8865
huangbin <876301469@qq.com>
2018-03-09 14:38:46 +0800

    c

---
+++ src/main/java/com/subway/workflow/WorkFlowInstance.java
@@ -0,0 +1,37 @@
+package com.subway.workflow;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by huangbin on 2018-3-3
+ * 工作流节点定义
+ */
+@Entity
+@Table(name = "T_WORK_FLOW")
+@Data
+public class WorkFlow implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", length = 11, nullable = false)
+    private Long id; //序号
+
+    @Column(name = "name", length = 20, nullable = false)
+    private String name; //工作流程名称
+
+    @Column(length = 50)
+    private String description; //工作流程名称
+
+    
+    @Column(length = 1, nullable = false)
+    private Long sortNo; //状态
+
+    @Column(length = 1, nullable = false)
+    private String status; //状态
+
+
+}
